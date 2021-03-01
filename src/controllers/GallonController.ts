b0de2098207762a940835cbaaa6dc0fa9de1662e
Renan <renanhc96@gmail.com>
2021-03-01 15:32:26 -0300

    Mocking Database

--- src/controllers/GallonController.ts
+++ src/controllers/GallonController.ts
@@ -1,11 +1,25 @@
 import { json, Request, Response } from "express";
+import { MockDatabase } from '../database/MockDatabase'
 
+const database = new MockDatabase();
 class GallonController {
 
   // Calculate the number of gallons needed from an area
   async query (request: Request, response: Response) { 
-    const test = { message: 'OK' }
-    return response.status(200).json(test);
+
+    const { area } = request.query;
+    const areaNum: number = +area;
+    const litersInk = areaNum / 5;
+    
+    const results = database.get(litersInk);
+
+    const payload = {
+      'area': (areaNum).toFixed(2),
+      'litersInk': (litersInk).toFixed(2),
+      ...results
+    }
+    
+    return response.status(200).json({ ...payload });
   }
 
   // Show a list of all gallons
